8c3099074e2f6873480888b5a0a4200533e59476
Richard Afro <richyafro@hotmail.com>
2019-06-06 11:03:42 +0100

    query notes and create notes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,83 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import gql from "graphql-tag";
+import { graphql } from "react-apollo";
+import { HalfCircleSpinner } from "react-epic-spinners";
 
-function App() {
+import Header from "./components/header";
+import NoteForm from "./components/note-form";
+import NoteList from "./components/note-list";
+
+import "./App.css";
+
+function App({ loading, notes }) {
+  const [stateNotes, setNotes] = useState([]);
+
+  useEffect(() => {
+    setNotes(notes);
+  }, [notes]);
+
+  const addNewNote = note => {
+    if (note.title && note.text) {
+      setNotes([...stateNotes, note]);
+    }
+  };
+
+  const removeNote = id => {
+    const filteredNotes = notes.filter(note => note.id !== id);
+    setNotes(filteredNotes);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <main className="App">
+      <Header />
+      <div className="container">
+        <div>
+          <NoteForm saveNewNote={addNewNote} />
+        </div>
+        <div className="" id="notes-box">
+          <div className="text-center">
+            <div className="header text-center">
+              {loading ? (
+                <div className="d-flex justify-content-center p-5">
+                  <HalfCircleSpinner size={50} color="grey" />
+                </div>
+              ) : (
+                <div>
+                  <NoteList notes={stateNotes} />
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
   );
 }
 
-export default App;
+const NOTE_LIST_QUERY = gql`
+  query {
+    notesList {
+      items {
+        id
+        title
+        text
+      }
+    }
+  }
+`;
+
+export default graphql(NOTE_LIST_QUERY, {
+  props: result => {
+    const {
+      data,
+      data: { loading }
+    } = result;
+    let notes = [];
+    if (data && data.notesList) notes = data.notesList.items;
+    console.log(data);
+    return {
+      loading,
+      notes
+    };
+  }
+})(App);
