63528d84c88e2cb22a2af268906ed14b04a94377
Richard Afro <richyafro@hotmail.com>
2019-06-06 11:05:20 +0100

    note card

---
+++ src/components/note-card/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+import DeleteButton from './delete-button';
+import "./note.css";
+
+const NoteCard = ({ note }) => (
+  <div className="note-card">
+    <div className="card text-white bg-card mb-3">
+      <div className="card-header">
+        <DeleteButton noteId={note.id}/>
+      </div>
+      <div className="card-body">
+        <h4 className="card-title note-title">{note.title}</h4>
+        <p className="card-text note-text">{note.text}</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default NoteCard;
