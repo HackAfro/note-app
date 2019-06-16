252beded490126ae844c4e9a1f78e5750f65623c
Richard Afro <richyafro@hotmail.com>
2019-06-16 16:57:52 +0100

    add rnd to make notes draggable

--- src/components/note-card/index.js
+++ src/components/note-card/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
+import { Rnd } from 'react-rnd';
 
 import DeleteButton from './delete-button';
 import './note.css';
 
-const NoteCard = ({ note, refetch }) => (
-  <div className="note-card">
-    <div className="card text-white bg-card mb-3">
-      <div className="card-header">
-        <DeleteButton noteId={note.id} refetch={refetch} />
+const NoteCard = ({ note, refetch }) => {
+  const position = {
+    x: Math.floor(Math.random() * (800 - 0 + 1) + 0),
+    y: Math.floor(Math.random() * (500 - 0 + 1) + 0),
+  };
+  
+  return (
+    <Rnd default={{ width: 250, height: 230, ...position }}>
+      <div className="note-card">
+        <div className="card text-white bg-card mb-3">
+          <div className="card-header">
+            <DeleteButton noteId={note.id} refetch={refetch} />
+          </div>
+          <div className="card-body">
+            <h4 className="card-title note-title">{note.title}</h4>
+            <p className="card-text note-text">{note.text}</p>
+          </div>
+        </div>
       </div>
-      <div className="card-body">
-        <h4 className="card-title note-title">{note.title}</h4>
-        <p className="card-text note-text">{note.text}</p>
-      </div>
-    </div>
-  </div>
-);
+    </Rnd>
+  );
+};
 
 export default NoteCard;
